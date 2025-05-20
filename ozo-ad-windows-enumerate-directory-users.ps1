8b8e28a044f5fe7ea699f2cd24364f4c3feab4e0
Andy Lievertz <alievertz@onezeroone.dev>
2025-05-20 12:08:39 -0600

    All features implemented; v1.0.0

--- ozo-ad-windows-enumerate-directory-users.ps1
+++ ozo-ad-windows-enumerate-directory-users.ps1
@@ -92,26 +92,172 @@ Class OZOMain {
     }
     # Report method
     Hidden [Void] Report() {
-        # Determine if any Items were created
-        If ($this.Items.Count -gt 0) {
-            # At least one Item was processed; output selected object properties to Excel
-            $this.Items | Select-Object -Property itemName,Validates,@{Name="Messages";Expression={$_.Messages -Join "; "}} | Export-Excel -WorksheetName $this.Json.ExcelWorksheetName -Path $this.excelPath
-            $this.Logger.Write(("See " + $this.excelPath + " for results."),"Information")
+        # Determine if any ChildItems were processed
+        If ($this.ChildItems.Count -gt 0) {
+            # At least one ChildItem was processed; produce the Item Detail sheet
+            $this.childItems | Select-Object -Property @{Name="Name";Expression={$_.childItem.Name}},
+            @{Name="Path";Expression={$_.ozoDirectorySummary.Path}},
+            @{Name="Last Modified";Expression={$_.ozoDirectorySummary.newestChildWriteTime}},
+            @{Name="Total size (GB)";Expression={($_.ozoDirectorySummary.totalSize / 1073741824)}},
+            @{Name="Long paths";Expression={[Boolean]$_.ozoDirectorySummary.longPaths}},
+            @{Name="Problem paths";Expression={[Boolean]$_.ozoDirectorySummary.problemPaths}},
+            @{Name="Messages";Expression={$_.Messages -Join "; "}} | Export-Excel -WorksheetName "Item Detail" -Path $this.excelPath
+            # Produce the sheet detailing all unique users found for all child items
+            $this.childItems.adObjects | Where-Object {$null -ne $_.adUser} | Select-Object -Unique -Property @{Name="Last Name";Expression={$_.adUser.Surname}},
+            @{Name="First Name";Expression={$_.adUser.GivenName}},
+            @{Name="Account";Expression={$_.adUser.SamAccountName}},
+            @{Name="Email Address";Expression={$_.adUser.EmailAddress}} | Sort-Object -Property "Last Name","First Name","Account" | Export-Excel -WorksheetName "Users" -Path $this.excelPath
+            # Produce the sheet detailing all unique groups found for all items
+            $this.childItems.adObjects | Where-Object {$null -ne $_.adGroup} | Select-Object -Unique -Property @{Name="Group Name";Expression={$_.adGroup.Name}} | Export-Excel -WorksheetName "Groups" -Path $this.excelPath
+            # Determine if there are any invalid SIDs
+            If (($this.childItems.adObjects | Where-Object {$_.Validates -eq $false}).Count -gt 0) {
+                # Found invalid SIDs; produce the sheet detailing all unique invalid SIDs
+                $this.childItems.adObjects | Where-Object {$_.Validates -eq $false} | Select-Object -Unique -Property @{Name="SID";Expression={$_.objectSID}},@{Name="Messasges";Expression={$_.Messages -Join "; "}} | Export-Excel -WorksheetName "Invalid Objects" -Path $this.excelPath
+            }
+            # Determine if there are any long paths
+            If (($this.childItems.ozoDirectorySummary.longPaths).Count -gt 0) {
+                # Found long paths on one or more objects; produce sheet detailing all long paths
+                $this.childItems.ozoDirectorySummary.longPaths | Select-Object -Property @{Name="Path";Expression={$_.FullName}},@{Name="Path Length";Expression={$_.FullName.Length}} | Export-Excel -WorksheetName "Long Paths" -Path $this.excelPath
+            }
+            # Determine if there are any problem paths
+            If (($this.childItems.ozoDirectorySummary.problemPaths).Count -gt 0) {
+                # Found problem paths on one or more objects; produce sheet detailing all problem paths
+                $this.childItems.ozoDirectorySummary.problemPaths | Select-Object -Property @{Name="Path";Expression={$_.FullName}},@{Name="Error Message";Expression={$_.ErrorMessage}} | Export-Excel -WorksheetName "Problem Paths" -Path $this.excelPath
+            }
+        } Else {
+            # No ChildItems were processed
+            $this.ozoLogger.Write("No child items were processed.","Warning")
         }
     }
 }
 
 Class OZOChildItem {
+    # PROPERTIES: FileSystemInfo
+    [System.IO.FileSystemInfo] $childItem = $null
+    # PROPERTIES: PSCustomObjects
+    [PSCustomObject] $ozoDirectorySummary = $null
+    # PROPERTIES: PSCustomObject Lists
+    [System.Collections.Generic.List[PSCustomObject]] $adObjects = @()
+    # PROPERTIES: String Lists
+    [System.Collections.Generic.List[String]]         $Messages  = @()
+    # METHODS
+    # Constructor method
+    OZOChildItem($ChildItem) {
+        # Set properties
+        $this.childItem = $ChildItem
+        # Get directory summary
+        $this.ozoDirectorySummary = (Get-OZODirectorySummary -Path $this.childItem.FullName)
+        # Iterate through the SIDs in the directory summary
+        ForEach ($objectSID in $this.ozoDirectorySummary.objectSIDs) {
+            # Determine if identity reference is a a domain ID
+            If ($objectSID -Like "S-1-5-21*") {
+                # objectSID is a domain ID; add an OZOADObject to the adObjects list
+                $this.adObjects.Add(([OZOADObject]::new($objectSID)))
+            }
+        }
+        # Iterate through the valid adObjects that contain an AD group
+        ForEach ($adObject in ($this.adObjects | Where-Object {$_.Validates -eq $true -And $null -ne $_.adGroup})) {
+            # Iterate through the members of the group, recusrively
+            ForEach ($groupMember in (Get-ADGroupMember -Identity $adObject.objectSID -Recursive)) {
+                # Try to get the SID for this user
+                Try {
+                    $userSID = (Get-ADUser -Ideneity $groupMember -ErrorAction Stop).SID
+                    # Success; determine if adObjects does not already contain this SID
+                    If ($this.adObjects.objectSID -NotContains $userSID) {
+                        # adObjects does not already contain an object with this SID; add it
+                        $this.adObjects.Add(([OZOADObject]::new($userSID)))
+                    }
+                } Catch {
+                    # Failure
+                    $this.Messages.Add(("While processing the " + $adObject.adGroup.Name + " [recursive] group members, unable to get SID for user with DN " + $groupMember + ". Error message is: " + $_))
+                }
+            }
+        }
+    }
+}
+
+Class OZOADObject {
     # PROPERTIES: Booleans, Strings
-    [Boolean] $Validates     = $true
-    [String]  $childItemName = $null
-    # PROPERTIES: Lists
+    [Boolean] $Validates = $true
+    [String]  $objectSID = $null
+    # PROPERTIES: PSCustomObjects
+    [PSCustomObject] $adGroup  = $null
+    [PSCustomObject] $adObject = $null
+    [PSCustomObject] $adUser   = $null
+    # PROPERTIES: String Lists
     [System.Collections.Generic.List[String]] $Messages = @()
     # METHODS
     # Constructor method
-    OZOItem($ChildItem) {
+    OZOADObject($ObjectSID) {
         # Set properties
-        $this.childItemName = $ChildItem
+        $this.objectSID = $ObjectSID
+        # Try to get the AD object
+        Try {
+            $this.adObject = (Get-ADObject -Filter {objectSid -eq $this.objectSID} -ErrorAction Stop)
+            # Success; switch on objectClass
+            Switch ($this.adObject.objectClass) {
+                "user" {
+                    $this.Validates = $this.GetADUser()
+                }
+                "group" {
+                    $this.Validates = $this.GetADGroup()
+                }
+                default {
+                    $this.Messages.Add(("ObjectClass " + $this.adObject.objectClass + " is not handled"))
+                    $this.Validates -eq $false
+                }
+            }
+        } Catch {
+            # Failure
+            $this.Messages.Add(("Unable to get AD object. Error message is: " + $_))
+        }
+    }
+    # Get AD User method
+    Hidden [Boolean] GetADUser() {
+        # Control variable
+        [Boolean] $Return = $true
+        # Determine that the object is valid and not null and class is User
+        If ($this.Validates -eq $true -And $null -ne $this.adObject -And $this.adObject.objectClass -eq "user") {
+            # Valid, not null, and user class; try to get the AD user
+            Try {
+                $this.adUser = (Get-ADUser -Identity $this.objectSID -Properties SamAccountName,GivenName,Surname,EmailAddress -ErrorAction Stop)
+                # Success; determine if user is disabled
+                If ($this.adUser.Enabled -eq $false) {
+                    # User is disabled
+                    $this.Messages.Add("User is disabled")
+                    $Return = $false
+                }
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Unable to get AD user object. Error message is: " + $_))
+                $Return = $false
+            }
+            # Make sure group is null
+            $this.adGroup = $null
+        }
+        # Return
+        return $Return
+    }
+    # Get AD Group method
+    Hidden [Boolean] GetADGroup() {
+        # Control variable
+        [Boolean] $Return = $true
+        # Determine that the object is valid and not null and class is Group
+        If ($this.Validates -eq $true -And $null -ne $this.adObject -And $this.adObject.objectClass -eq "group") {
+            # Valid, not null, and group class; try to get the group
+            Try {
+                $this.adGroup = (Get-ADGroup -Identity $this.objectSID -ErrorAction Stop)
+                # Success
+            } Catch {
+                # Failure
+                $this.Messages.Add(("Unable to get AD group object. Error message is: " + $_))
+                $Return = $false
+            }
+            # Make sure user is null
+            $this.adUser = $null
+        }
+        # Return
+        return $Return
     }
 }
 
